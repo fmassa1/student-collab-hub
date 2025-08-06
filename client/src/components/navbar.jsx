20d6ee36501f79aba4051a1ecd4b419f4cf3c4ca
Fredric Massa <rickmass2020@gmail.com>
2025-08-05 20:24:32 -0500

    changed login button

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -9,8 +9,8 @@ function NavBar() {
                     <li><a href="/projects">Browse Projects</a></li>
                     <li><a href="/">About</a></li>
                     <li><a href="/contact">Contact</a></li>
-                    <li><a href="#login">Login</a></li>
-                    <li><a href="#signup" className="cta-button">Sign Up</a></li>
+                    <li><a href="#login" className="login-button">Login</a></li>
+                    <li><a href="#signup" className="signup-button">Sign Up</a></li>
                 </ul>
             </div>
         </nav>
