20a5e6c3c32457271474c6101e69efa974d1889a
Fredric Massa <rickmass2020@gmail.com>
2025-08-11 14:13:12 -0500

    added app.css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+
 import Home from './pages/Home/home';
 //import About from './pages/about';
 import Contact from './pages/Contact/contact';
@@ -7,6 +8,7 @@ import Projects from './pages/Projects/projects';
 import Navbar from './components/NavBar/navbar';
 import Footer from './components/Footer/footer';
 
+import './styles/App.css'
 
 function App() {
   return (
