b015ceb30cfe9282d1b1a7db8c1e4a40eaff0713
Fredric Massa <rickmass2020@gmail.com>
2025-08-14 14:00:01 -0500

    added userController functions

---
+++ backend/controllers/usersController.js
@@ -0,0 +1,51 @@
+const projectsModel = require('../models/usersModel');
+
+async function addNewUser(req, res) {
+    try {
+        const { email, username, password, first_name, last_name, university } = req.body;
+        
+        const new_user = await usersModel.addNewUser({
+            email,
+            username,
+            password,
+            first_name,
+            last_name,
+            university
+        });    
+
+        res.status(201).json(new_user);
+
+    } catch (err) {
+        console.error('Error signing up user: ', err);
+        res.status(500).json({ error: 'Database error' });
+    }
+}
+
+async function getAllUsers(req, res) {
+    try {
+        const users = await usersModel.getAllUsers();
+        res.json(users);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Database error getAllUsers' });
+    }
+}
+
+async function getUserById(req, res) {
+    try {
+        const id = req.params.id;
+        const  cur_user = await usersModel.getUserById(id);
+        res.json(cur_user);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Database error getUserById' });
+    }
+}
+
+
+
+module.exports = {
+    addNewUser,
+    getAllUsers,
+    getUserById,
+};
