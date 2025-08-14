2eb480c5389f130371b702d975f180162fee0796
Fredric Massa <rickmass2020@gmail.com>
2025-08-14 12:40:28 -0500

    updated database, added user and like tables, and updated others

--- backend/sql/db_schema.sql
+++ backend/sql/db_schema.sql
@@ -1,26 +1,47 @@
 CREATE DATABASE IF NOT EXISTS peer_spark;
 USE peer_spark;
 
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    user_name VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    university VARCHAR(100) NOT NULL
+)ENGINE=InnoDB;
+
 CREATE TABLE projects (
     id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
     name VARCHAR(255) NOT NULL,
     image_url VARCHAR(500),
     description TEXT,
     linkedin_url VARCHAR(500),
-    github_url VARCHAR(500)
-);
+    github_url VARCHAR(500),
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+)ENGINE=InnoDB;
 
 CREATE TABLE project_tags (
     id INT AUTO_INCREMENT PRIMARY KEY,
     project_id INT NOT NULL,
     tag VARCHAR(100) NOT NULL,
     FOREIGN KEY (project_id) REFERENCES projects(id) ON DELETE CASCADE
-);
+)ENGINE=InnoDB;
 
 CREATE TABLE project_comments (
     id INT AUTO_INCREMENT PRIMARY KEY,
     project_id INT NOT NULL,
-    user VARCHAR(100) NOT NULL,
+    user_id INT NOT NULL,
     comment VARCHAR(100) NOT NULL,
-    FOREIGN KEY (project_id) REFERENCES projects(id) ON DELETE CASCADE
-);
+    FOREIGN KEY (project_id) REFERENCES projects(id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+)ENGINE=InnoDB;
+
+CREATE TABLE project_likes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    project_id INT NOT NULL,
+    user_id INT NOT NULL,
+    FOREIGN KEY (project_id) REFERENCES projects(id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+)ENGINE=InnoDB;
